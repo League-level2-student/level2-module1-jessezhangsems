248d3db37ca9e798aad55984a37ad2f555127947
student <student@ip-172-31-55-209.us-west-2.compute.internal>
2021-09-08 16:21:13 -0700

    array

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -1,10 +1,23 @@
 package _00_intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
+		ArrayList<String> strings = new ArrayList<String>();
+		
+		
 		//   Don't forget to import the ArrayList (java.util) class
+strings.add("one");
+strings.add("two");
+strings.add("three");
+strings.add("four");
+strings.add("five");
 
+for (int i = 0; i < args.length; i++) {
+	
+}
 		//2. Add five Strings to your list
 		
 		//3. Print all the Strings using a standard for-loop
